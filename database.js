b485b05ab8bd1a4d50170fe1638751ffe6c71df7
yousufaayman <135641264+yousufaayman@users.noreply.github.com>
2025-05-01 13:51:16 +0300

    Update database.js
    Persistent path for SQlite db for deployment on Render.

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 
-const dbPath = path.resolve(__dirname, 'judgment.db');
+const dbPath = path.resolve(__dirname, 'data', 'judgment.db');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
